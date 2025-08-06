79fc86d9f9b3485baaf3f4aa788efbe2ec36e7ca
gleeon <68011423@kmitl.ac.th>
2025-08-06 14:03:50 +0000

    commit

--- Lab-3/ex02.c
+++ Lab-3/ex02.c
@@ -3,12 +3,11 @@ int main(){
     int num, n;
     printf("Enter a number : ");
     scanf("%d", &num);
-    n =num%2;
     if(num>100){
         printf("%d is out of range\n", num);
     }
     else {
-        if (n==0){
+        if ((num%2)==0){
             printf("%d is even\n", num);
         }
         else {
--- Lab-3/ex03.c
+++ Lab-3/ex03.c
@@ -3,11 +3,11 @@ int main(){
     int num;
     printf("Enter a number : ");
     scanf("%d", &num);
-    switch(num<=100&&num>= 1){
-        case 0:
+    switch(num>100){
+        case 1:
             printf("%d is out of range", num);
             break;
-        case 1:
+        case 0:
             switch(num%2){
                 case 0:
                     printf("%d is even\n", num);
